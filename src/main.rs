80ece120362cccc9a4f17db7c2bf83130b3b7b7b
Oleksii Panpukha <58779018+3xecut0r@users.noreply.github.com>
2026-01-01 21:42:54 +0100

    fix: replaced panic with stderr + optimized (#2)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::path::{Path};
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result, Write, IsTerminal};
 use std::io;
+use std::process;
 
 
 fn main() -> Result<()> {
@@ -16,50 +17,56 @@ fn define_workflow() -> Result<()> {
     // 1. pattern = args[0], input = stdin
     // 2. path = args[0], pattern = args[1], input = file
     // else -> print usage and std::process::exit(2)
+    let args: Vec<String> = env::args().skip(1).collect();
 
     // 1
     if !io::stdin().is_terminal() {
         // stdin = pipe / redirect
-        let reader = BufReader::new(io::stdin().lock());
-        let pattern = env::args().skip(1).next();
-        if pattern.is_none() {
-            panic!("There's no pattern")
+        let stdin = io::stdin().lock();
+        let reader = BufReader::new(stdin);
+
+        if args.len() < 1 {
+            usage("There's no pattern", 2);
         }
-        let pattern = pattern.unwrap();
-        let _ = grep(reader, &pattern);
+        let pattern = &args[0];
+        let _ = grep(reader, pattern);
         return Ok(())
     }
 
     // 2
-    let path = env::args().skip(1).next();
-    if path.is_none() {
-        panic!("There's no path")
+    if args.len() < 1 {
+        usage("There's no path", 2);
     }
-    let path = path.unwrap();
+    let path = &args[0];
     if Path::new(&path).exists() {
-        let pattern = env::args().skip(2).next();
-        if pattern.is_none() {
-            panic!("There's no pattern")
-        }
-        let pattern = pattern.unwrap();
 
+        if args.len() < 2 {
+            usage("There's no pattern", 2);
+        }
+        let pattern = &args[1];
         let file = File::open(&path)?;
         let reader = BufReader::new(file);
-        let _ = grep(reader, &pattern);
+        let _ = grep(reader, pattern);
         return Ok(())
     }
 
     Ok(())
 }
 
-fn grep<R: BufRead>(mut reader: R, pattern: &str) -> Result<()> {
+fn grep<R: BufRead>(reader: R, pattern: &str) -> Result<()> {
+    let mut out = io::stdout().lock();
     for line in reader.lines() {
         let line = line?;
         if line.contains(pattern) {
-            let mut out = io::stdout().lock();
             out.write_all(line.as_bytes())?;
             out.write_all(b"\n")?;
         }
     }
     Ok(())
 }
+
+fn usage(message: &str, code: i32) {
+    eprintln!("{}", message);
+    process::exit(code);
+}
+
